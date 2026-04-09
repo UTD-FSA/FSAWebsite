ecd8b41c339e83bf3b624f203ed8fc9974919778
KarstonC <161514825+KarstonC@users.noreply.github.com>
2026-04-09 15:32:47 -0500

    added basic layout for pam page

--- utdfsa/app/Pamilya/page.tsx
+++ utdfsa/app/Pamilya/page.tsx
@@ -1,16 +1,36 @@
-<<<<<<< HEAD
-export default function PamilyaPage() {
-  return (
-    <div>
-      <h1>Pamilya Page</h1>
-      <p>This is the Pamilya page.</p>
-    </div>
-=======
 export default function Pamilya() {
   return (
-    <main>
-      <h1>Pamilya</h1>
-    </main>
->>>>>>> origin/Patrick
+    <div>
+      <main>
+        <h1>Get Involved!</h1>
+        <p>Interested in becoming a member? UTD FSA is always looking for new members to be apart of our pamilya! Whether its sports, dance, culture or any of our other programs, we offer a variety of ways to get involved! Fill out our new member form and get access to all our exciting events and activities in the upcoming semesters. So what are you waiting for -- SIGN UP TODAY!  -- COPIED FROM UTFSA</p>
+          <h2>What is a pam? ading? kuya? ate? -- IDK HOW TO FORMAT THIS SOMEONE FIGURE IT OUT</h2>
+            <p>pam - group of people!
+                kuya/ate - group leader
+                ading - group member
+            </p>
+          <h2>Meet the Pams! -- ADD A PICTURE FROM PAM REVEALS AND MAYBE A KUYATE LIST?</h2>
+            <h3>Shiballers</h3>
+            <h3>Gutom Gang</h3>
+            <h3>Sushi Cuchi</h3>
+            <h3>Hanobe</h3>
+            <h3>Moganda</h3>
+            <h3>SDIYBT</h3>
+            <h3>Arigyattos</h3>
+          <h2>Want to join FSA?</h2>
+            <p>Fill out the member form!</p>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfi9GCyrFiCrCkhE8YY5LdZHtZ2eTNojX2aIjaQ7niVaoCVzA/viewform?embedded=true" 
+              width="640" 
+              height="1210" 
+              frameBorder="0"
+              marginHeight={0}
+              marginWidth={0}
+            >
+              Loading…
+            </iframe>
+          <h2>Interested in becoming a Kuya/Ate?</h2>
+            <p>Fill out the Kuya/Ate form!</p>
+      </main>
+    </div>
   );
 }
